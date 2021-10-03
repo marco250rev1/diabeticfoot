3d4722ec6324bd35531ba984046009f4489ba298
marco250rev1 <marco250_rev@hotmail.com>
2021-10-02 22:59:22 -0500

    Initial Commit

---
+++ Shared/diabeticfootApp.swift
@@ -0,0 +1,17 @@
+//
+//  diabeticfootApp.swift
+//  Shared
+//
+//  Created by Marco Ruiz on 02/10/21.
+//
+
+import SwiftUI
+
+@main
+struct diabeticfootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
